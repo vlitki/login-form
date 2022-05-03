92063d6b1f6adf75cb6f91671dfd05dcb34755c5
vlitki <macanicv@gmail.com>
2022-05-03 21:05:20 +0200

    login form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
+import React, {useState, useEffect } from "react"
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  const [name, setName] = useState('');
+  const [userName, setUserName] = useState('');
+  const [location, setLocation] = useState('')
+  const [avatar, setAvatar] = useState('')
+  const [bio, setBio] = useState('')
+  const [input, setInput ] = useState('')
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/example")
+    .then(res => res.json())
+    .then(data => {
+      setData(data);
+    })
+
+  }, []);
+
+  const setData = ({avatar_url, name, login, location, bio}) => {
+    setName(name)
+    setUserName(login)
+    setLocation(location)
+    setAvatar(avatar_url)
+    setBio(bio)
+
+  }
+  const handleSearch = (e) => {
+    setInput(e.target.value)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='navbar'>Github Search</div>
+      <div className="search">
+        <form>
+          <formwrap>
+            <input placeholder="Github user" name="github-user" />
+            <button content="Submit">Submit</button> 
+            <img src={avatar} />
+            <div>{bio} Bio</div> 
+          </formwrap>
+        </form>
+      </div>
     </div>
   );
 }
